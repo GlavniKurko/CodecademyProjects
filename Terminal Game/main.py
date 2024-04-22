9457805d70f9b8560aba852b6606c67bbab51115
GlavniKurko <armingradascevic32@gmail.com>
2024-04-22 10:44:27 -0400

    Updated script to determine winner after 3+ moves are made, with a total of 5.

--- Terminal Game/main.py
+++ Terminal Game/main.py
@@ -22,9 +22,9 @@ def checkWinner(user1, user2):
     global gameboard
     gameboard_string = gameboard[1]
     winning_positions = {1: ["1","2","3"], 2: ["1","5","9"], 3: ["1","4","7"], 4: ["2","5","8"], 5: ["3","6","9"], 6: ["3","5","7"], 7: ["4","5","6"], 8: ["7","8","9"]}
-    if len(user1) < 3 and len(user2) < 3:
+    if len(user1) < 3 or len(user2) < 3:
         update_board(user1, user2)
-    elif len(user1) == 3 and len(user2) == 3:
+    elif len(user1) == 3 or len(user2) == 3:
         user1_list = user1
         user2_list = user2
         user1_list.sort()
@@ -41,7 +41,7 @@ def checkWinner(user1, user2):
             else:
                 pass
         update_board(user1, user2)
-    else:
+    elif len(user1) == 4 or len(user2) == 4:
         user1_list_2 = user1
         user2_list_2 = user2
         for x in winning_positions.values():
@@ -55,6 +55,21 @@ def checkWinner(user1, user2):
                 sys.exit()
             else:
                 pass
+    elif len(user1) == 5:
+        user1_list_2 = user1
+        user2_list_2 = user2
+        for x in winning_positions.values():
+            if x[0] in user1_list_2 and x[1] in user1_list_2 and x[2] in user1_list_2:
+                print("User1 has won!")
+                update_board(user1_list_2, user2_list_2)
+                sys.exit()
+            else:
+                print("The Game is a draw!")
+                update_board(user1_list_2, user2_list_2)
+                sys.exit()
+    else:
+        user1_list_2 = user1
+        user2_list_2 = user2             
         print("The Game is a draw!")
         update_board(user1_list_2, user2_list_2)
         sys.exit()
@@ -81,6 +96,7 @@ def main():
     while game_over != True:
         user1_input = str(input("P1 Pick a Square: "))
         user1_moves.append(user1_input)
+        checkWinner(user1_moves, user2_moves)
         user2_input = str(input("P2 Pick a Square: "))
         user2_moves.append(user2_input)
         checkWinner(user1_moves, user2_moves)
