9f54c154bde5c99d97e344654cca4f269b191c19
GlavniKurko <armingradascevic32@gmail.com>
2024-04-22 00:19:04 -0400

    Created function to update the gameboard as the game is played.

--- Terminal Game/main.py
+++ Terminal Game/main.py
@@ -1,47 +1,51 @@
-def user_move(player, move):
-    if move == "1" and player == 1:
-        print('''
-                             |            |           
-                             |            |           
-                     X       |      2     |    3      
-                 ____________|____________|___________
-                             |            |           
-                             |            |           
-                     4       |      5     |    6      
-                 ____________|____________|___________          
-                             |            |           
-                             |            |           
-                     7       |      8     |    9      
-                 ____________|____________|___________
-                             |            |
-                             |            |
-                             |            |''')
+game_over = False
+gameboard = {1: '''          
+                         |           |             
+                         |           |             
+                  1      |     2     |    3     
+             ____________|___________|__________
+                         |           |          
+                         |           |          
+                  4      |     5     |    6     
+            _____________|___________|__________
+                         |           |
+                         |           |
+                  7      |     8     |    9
+             ____________|___________|__________
+                         |           |
+                         |           |'''}
+ 
+def update_board(user1, user2):
+    global gameboard
+    gameboard_string = gameboard[1]
+    for x in user1:
+        gameboard_string = gameboard_string.replace(x, "X")
+    for x in user2:
+        gameboard_string = gameboard_string.replace(x, "O")
+    
+    print(gameboard_string)
 
-def gameboard():
-    print('''
-                         |            |           
-                         |            |           
-                 1       |      2     |    3      
-             ____________|____________|___________
-                         |            |           
-                         |            |           
-                 4       |      5     |    6      
-             ____________|____________|___________          
-                         |            |           
-                         |            |           
-                 7       |      8     |    9      
-             ____________|____________|___________
-                         |            |
-                         |            |
-                         |            |''')
+def get_gameboard():
+    global gameboard
+    print(gameboard[1])
+
 def main():
-    game_over = False
-    user_moves = []
-    gameboard()
-
+    global game_over
+    get_gameboard()
+    user1_moves = []
+    user2_moves = []
     while game_over != True:
-        user1_input = input("Enter a number: ")
-        user_move(1, user1_input)
+        user1_input = str(input("User1 Pick a Square: "))
+        user1_moves.append(user1_input)
+        user2_input = str(input("User2 Pick a Square: "))
+        user2_moves.append(user2_input)
+
+        update_board(user1_moves, user2_moves)
+
+
+
+
+
 
     
 
@@ -52,7 +56,7 @@ print(''' _____  _  ____     _____  ____  ____     _____  ____  _____
   / \  | ||  / _____ / \  | / \||  / _____ / \  | / \||  \    
   | |  | ||  \_\____\| |  | |-|||  \_\____\| |  | \_/||  /_   
   \_/  \_/\____/     \_/  \_/ \|\____/     \_/  \____/\____|''')
-user_input = input("Do you want to play a game of BlackJack? (Yes or no): ")
+user_input = input("Do you want to play a game of Tic-Tac-Toe? (Yes or no): ")
 if user_input.lower() == "yes":
     main()
 else:
